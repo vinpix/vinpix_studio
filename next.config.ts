7b4c403b41e1298bc6f3436773b7e977be0503d5
vinpix <kiet57441@gmail.com>
2025-08-28 03:05:17 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    // Disable ESLint during production builds (Vercel)
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
